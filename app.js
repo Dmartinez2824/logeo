2237cdd20cd232116f8729aaba1edf14bb038fb1
helder amado <hfelipeavillareal@gmail.com>
2025-05-23 20:51:25 -0500

    ERRORES EN INTERPRETACION FALLAS EN IMPORTAR LAS LIBRERIAS

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import bodyParser from 'body-parser';
 import express from 'express';
-import bcrypt from 'bcrypt.js';
+// import bcrypt from 'bcrypt';
+import mysql from 'mysql';
+
 
  const app = express();
 
@@ -9,12 +11,27 @@ import bcrypt from 'bcrypt.js';
 app.use(express.urlencoded({extended:true}))
 
  app.post('/login', (req,res) =>{
-   const email = req.body.email;
+   const correo = req.body.correo;
    const password = req.body.password;
-   const contrasena = bcrypt.hash(password,10);
+   const contrasena = bscrypt.hash(password,10);
+   const respuesta = conexion.query('select * from usuarios where correo = ?', [correo])
     console.log(req.body);
     
     return res.json({message: 'Hola mundo'});
  });
 
+ app.post('/registro',async (req, res) => {
+ const nombre = req.body.nombre;
+ const correo = req.body.correo;
+ const contrasena = await bscrypt.hash(password,10);
+ const respuesta = conexion.query('insert into usuarios (nombre, correo, constrasena) values (?,?,?), [nombre, correo, constrasena]|')
+ return  res.json({"registro" : true});
+ })
+
  app.listen(3000);
+ const conexion = mysql.createConnection({
+  host: "localhost",
+  user: "Daniel_Martinez",
+  password: "ADSO",
+  database: "Daniel Martinez"
+ })
